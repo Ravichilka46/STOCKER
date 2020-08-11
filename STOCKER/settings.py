dc1e9220b6bdc200f3bf9b0677a0dc36d47f2ffa
RAVIKISHOR <ravichilka46@gmail.com>
2020-08-11 11:48:12 +0530

    set Commit

--- STOCKER/settings.py
+++ STOCKER/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = 'un-t-@n6*@q5ldzkz-#ldmqfb3+3k%w*5*#sf3m^6ke1_=fm(='
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
